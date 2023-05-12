d8b846c05d9f312bed6ddc7bf4140cb576301d82
Arturo Guzmán Lucena <arturo.s238739@cesurformacion.com>
2023-05-12 11:01:25 +0200

    Docker

--- src/main/java/javacode/server/springelectronriftstatisticswebapp/HtmlFactory/HtmlFactory.java
+++ src/main/java/javacode/server/springelectronriftstatisticswebapp/HtmlFactory/HtmlFactory.java
@@ -30,16 +30,24 @@ import no.stelar7.api.r4j.pojo.lol.staticdata.item.Item;
 import no.stelar7.api.r4j.pojo.lol.summoner.Summoner;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.StringWriter;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.net.JarURLConnection;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.function.Supplier;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class HtmlFactory {
     private static final String DEFAULT_USER_IMAGE = "iVBORw0KGgoAAAANSUhEUgAAAEMAAABDCAYAAADHyrhzAAAACXBIWXMAAAsTAAALEwEAmpwYAAAJZklEQVR4nO1cabAcVRXuPBF3RUHcEmKFedPn+87MvOhTiRh9Ai6IuIHRQi0qKIJLUbgh5YIoRsWwVAlFUFwQTCgrKCgoVgCVTRQNxqqogFoiBogS8gAJvITlxTo93dO3e3omM93T700oTtX86eXe29892/3uueN5j8uMywjJhQp8gJSvK+ViEmsV8neFbCRkksTtStyikBuUspqUZTXgfSJS9XZ2qVQqzyR5BIEfK3G3Etvz/gwoUs5X9Q+tVCpP8nYWAfBqBVaReLAIAB2BgUwq5Jt13697wyokD1TKNV1m9xElbiRxngKfJeUwVf+NJBfXRcYBvAbAm8w0FPgSKT9UyF+UmO7Q5rRSfqKqL/eGRUSkSuLyzAFDNpOyQlUOHl+w4Fl52q/Vas9T4N0KWUnIA1mgkPIDe86bLZmYmNjFZlCJrRlacJWqHEJy107vz58//8ljo6MvMjBNM0hWROQF3d7xff8ZNeD9SqzPMJ97SB7tzbSo6jwC17XPklxhap/14ar+fgqcRGCNUv7VSf0JediiDIFLzZxqIq804FNNzgHwNoWsy9DGC/NqYQ4g/NemowMhG0i+Mz1gkvsr8H2F3F/QcW5S4My0f1jieU8geQyJe1Pj+YeqomQg5BAlppImIeeb+jqPjaj6byHwh3KiCa6z9g3sqEPf919I4FepZ+8WkUWlAEH6S8OIEPmFB81+3WfM9ptJ0+BByPJLJOlqiQJfVOJRR0O2ADhgoEBoMNPycMpZLU74BFNjZyAz8oNsM79i2thJey0Cmd8ZXBJFxzQgG0WkFt23KKCUP84oCO1acnmj0dgzGlNN5PVuGCbkvzbOQkDULMYTdzod32drjPi+/zISd80mEA4g/wQw6kzi6wLNiZ9ZPz4+/tRcQCwJbFB+6TQ2ZVriaMT+A4gSgzabhNaqyOGJEA6ckwsMBT6d7AgfdjVCif/N+sdn/+7wff/F8aTJN9z7AN7eHxCq8xKzDrkwugdg/rCYRheTuTlKvMbHx59I4vrWPci/ST69dzAoFzsN30XyOVHDSvy24EAfaYZF+USQwKliYaXyXABqjk+BzwdcR+cFWo8/We1MLhL+Azi5JyAA7JMcvL+01SjwtSIgKHAuyb16GYfZPomfFQNePuiM/WTn3lZL1HY4CAKXOOj+Lsr0gsFBHso1KMgG8zNezhzHoljOfiejkNtoNJ6WiIzAqV07rtWqjYR6qhzcGhTl6pwasZbk870CYplmuLjrX0Mg34vb8T/pALVFRHbvPAuQM5yH/xRpRRCz8w3ktkHxDGb36QVZr+YZ8ajmON1Fpi3yMjsbb3rdVpQg5agWou2LoF5U9CEALxkEEKlUO4/v+G6rDeA0Z4y/72ab28Pf1NjY2G4hmsxpHmcNEojWOClX5NDQbZFJqOqYe6/u+35bJyTOihHDj+LrRu33DcZUUT/RSUi+It/kxCxYgikT+Vh7J8BNzgNHtl6E3Na/ieDSMoBwPuaWHJp6VfxNONXRmp8mGjfuUZMoBis8U6E8s2CbRWWCkUtbIdssvNr7AA5yQLrX1mEpmh/Ri3c614/OqZILywWDS3JNksobog2uZAqhe7caV+DY+CW5Mu4U387TqaXXZYKhWn1VvkmSz7S+DbIhum77NQ7SsiIrCuRNtBJqV4JY3pAPDJznfNuVmU7UXZgpcFx8PUHs9GMmva8Kc0iwiZ1nXMBvWt8GnONozCleFkp0kq2sDaLebLNcmt44iZxg3JQVUUg5OwbDYbRJOcxZrvcPBLHd9kvLBIOUL+c0k9udNk6MrUFWumCsi2fVP9RZ5eUCwzaNywSjAAG9yQHj+Kwk00tsFYocHl4eKUCwTJUVUUTkpXknyVa+DhjLYs3AuTHSkF84s/pR53puwpeU5WWAkSKp+/2tz+RGIWe0OghqIdj6iBPj67g5d8eQbcaPDBIIku8tAISZw5oYVKxyvnmZl0nnAatandtOeIHObQ0R8adFJSCeIJsLjQc4MwY24FlDMHiEg7i/NL6BtfF1WV4QjIAz6Moo9VoQA/lP0bGoyEeiNhNUovr7up0tcgb/QFQwQvKthQcQakhqp75nsfdst2wQ44jqwGwt4l5PTJZtHNMpSIt2zur1+rMLbyZDri3KeNlCspD/isNqQGMaa+5OVFcvTdeJAr/OX67ov8etoygIyK4KfCovU+4SwwpckJl9Og98znlxXTwIOSpHxyvLKiWyOrA84dU2qFpWALknBoNL2sFQReLlMCwaF9pHvrE14ZnLkxFSTugjKbw1Wkm7XIi5ho6TRrf0CDitBRRweg/asDmruK1MsaVDuqwq22TjLYFEugBc0LFxksc4jdxnDrTZqc7r2ilks6XJ3iyIsVc7GNvGqC7D9nMTZU7kgd3rvSGTjnM5ITOXT5lGaYVkPUpQBtkh6rl1Z4msE/JXt/wpUxQ4yXlhMlpwBavYDKY8XeQ2W0LKFzK04obog22/xC3Q64lmEJHdE+HLWdFZ/uE2qJTLvOGREdsodyZyS61WlfDeHGO5nHH/LaPQNltI+biD8HRNZCLDXLZa4Yo3RGJFs1GEcTU2lWRZUnlQn3Xh0iJ8jEmuVqt7hLdHgjMkLgcwRBJsPzqRMCSPWyVXJC7qu1EA+yTrMeTnkf0tmjv3KfX66AJvCCXccR+J2Tr5s+sDLTLmaliB45LxWlZ4O4kE1You698093cUaXNOG6fRrMgddpljZpwa9+mFW/WbS+i1qYZ7KxCbBTF/R8p3EhoNXNJz9NiRNBqNPYOThkmTOXtgHQxIbOJcPjfMNa41HzfQjkjulShbCDuq1ytzvSGQsAglVaogV5d2EKdare7hJjbhbxOAd3mzJOHRimPbz8HgIluul9p5o5mWr2xbGQJrZvpArh2dyDiaNa3AV8ve/E6IbTZZypsayKNWmVv2XqsRuGGUm87Q0t6zy0EKgNHmAby21aKBcpnRfrmPNaTEthxqwIfcOnBXG+x42Kwe54zE2KOODDbkfptFW+/Ysa1e7TiIDOrvG9CRTZqvU0XAjXZA2BsmmbD1TGA6qYiTqTW4tUk0y2ojahX4lhWSNM/NBwUyd+ygDXOQ19dE3jwowrnMf0RYbB848DMptqMWADez9OJAxPxFc89Dlhu/2m8hfnMvR66xU4lmClYz4j1WZGJiYhdzulag33SGcjwpXwmOPxjDZqefRI40XjM8TdSdlntcvNLk/8aP+9I3HThCAAAAAElFTkSuQmCC";
@@ -53,7 +61,46 @@ public class HtmlFactory {
         DataCall.setCacheProvider(fileCache.get());
         cfg = new Configuration(Configuration.VERSION_2_3_31);
 
-        FileTemplateLoader fileTemplateLoader = new FileTemplateLoader(new File("src/main/java/javacode/server/springelectronriftstatisticswebapp/HtmlFactory/templates"));
+        List<InputStream> inputList = new ArrayList<>();
+        Enumeration<URL> resources = getClass().getClassLoader().getResources("templates/");
+        while (resources.hasMoreElements()) {
+            URL url = resources.nextElement();
+            if (url.getProtocol().equals("jar")) {
+                JarURLConnection jarURLConnection = (JarURLConnection)url.openConnection();
+                JarFile jarFile = jarURLConnection.getJarFile();
+                for (JarEntry jarEntry : Collections.list(jarFile.entries())) {
+                    if (jarEntry.getName().startsWith("templates/") && !jarEntry.isDirectory()) {
+                        inputList.add(jarFile.getInputStream(jarEntry));
+                    }
+                }
+            } else {
+                inputList.add(url.openStream());
+            }
+        }
+
+        File tempFolder = new File("tempTemplates");
+        if (!tempFolder.exists()) {
+            tempFolder.mkdir();
+        }
+
+        for (int i = 0; i < inputList.size(); i++) {
+            InputStream inputStream = inputList.get(i);
+            String fileName = "template" + i;
+            File outputFile = new File(tempFolder, fileName);
+            FileOutputStream outputStream = new FileOutputStream(outputFile);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.close();
+            inputStream.close();
+            String originalFileName = new File(tempFolder.getPath() + "/" + "template" + i).toString();
+            File originalFile = new File(tempFolder, originalFileName);
+            outputFile.renameTo(originalFile);
+        }
+
+        FileTemplateLoader fileTemplateLoader = new FileTemplateLoader(tempFolder);
         cfg.setTemplateLoader(fileTemplateLoader);
     }
 
@@ -104,7 +151,7 @@ public class HtmlFactory {
                 if (initialization) {
                     template = cfg.getTemplate("cleanUnloggedIndex.ftl");
                 }else {
-                 template = cfg.getTemplate("unloggedIndex.ftl");
+                    template = cfg.getTemplate("unloggedIndex.ftl");
                 }
                 StringWriter out = new StringWriter();
                 template.process(null, out);
